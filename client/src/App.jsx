32d9c864825beb117c884c8ef442134a9c6d91e2
Tim La <tlamarketing111@gmail.com>
2023-01-24 09:09:08 -0500

    wrapped app in browserrouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,11 @@ import { Home, CreatePost } from './pages';
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <BrowserRouter>
+      <header>
+
+      </header>
+    </BrowserRouter>
   )
 }
 
