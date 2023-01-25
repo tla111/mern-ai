5594d40c0478f28f468a26768662e2f0ddf9c7f2
Tim La <tlamarketing111@gmail.com>
2023-01-25 07:49:09 -0500

    set up states in home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import { Card, FormField, Loader } from '../components';
 
 const Home = () => {
+    const [loading, setLoading] = useState(false);
+    const [allPosts, setAllPosts] = useState(null);
     return (
         <div>Home</div>
     )
