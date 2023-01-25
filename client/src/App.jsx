0a2038f9a99c29e104e2084d3f61227fee019d7d
Tim La <tlamarketing111@gmail.com>
2023-01-25 07:29:48 -0500

    styled header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import { Home, CreatePost } from './pages';
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
 
       </header>
     </BrowserRouter>
