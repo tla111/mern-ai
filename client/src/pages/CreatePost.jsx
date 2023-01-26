c32ee083cb1ae13177c1d60cfb2a946c26703f7b
Tim La <tlamarketing111@gmail.com>
2023-01-26 06:32:18 -0500

    implemented form tag

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,12 +17,20 @@ const CreatePost = () => {
     const [generatingImg, setGeneratingImg] = useState(false);
     const [loading, setLoading] = useState(false);
 
+    const handleSubmit = () => {
+
+    }
+
     return (
         <section className="max-w-7xl mx-auto">
             <div>
                 <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
                 <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
             </div>
+
+            <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+
+            </form>
         </section>
     )
 }
