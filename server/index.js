28bb8a59b3cf01100bbd8decc5ad1c51b245939c
Tim La <tlamarketing111@gmail.com>
2023-01-31 07:35:36 -0500

    use endpoints

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
 app.get('/', async (req, res) => {
     res.status(200).json({
         message: 'Hello from DALL.E!',
