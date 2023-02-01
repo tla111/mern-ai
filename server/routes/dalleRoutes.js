8485dff39e98e0328e63a7e9990e40737ef3c32a
Tim La <tlamarketing111@gmail.com>
2023-02-01 06:45:39 -0500

    connected to openai

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -5,3 +5,15 @@ import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 const router = express.Router();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+router.route('/').get((req, res) => {
+    res.status(200).json({ message: 'Hello from DALL-E!' });
+});
+
+export default router;
